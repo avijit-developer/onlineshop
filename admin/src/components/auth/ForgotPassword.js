fbb67a99ad46528fc6142d26a495df43ef028567
sumitra-designer <sumitramahanty89@gmail.com>
2026-04-19 12:12:54 +0530

    test5

--- admin/src/components/auth/ForgotPassword.js
+++ admin/src/components/auth/ForgotPassword.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import './Login.css';
-
+ 
 
 const ORIGIN = (typeof window !== 'undefined' && window.location) ? window.location.origin : '';
 const API_BASE = process.env.REACT_APP_API_URL || (ORIGIN && ORIGIN.includes('localhost:3000') ? 'http://localhost:5000' : (ORIGIN || 'http://localhost:5000'));
